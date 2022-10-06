67ef6826c62861f1432221a0d34b19ba9d9f4487
youthink0 <a0985418957@gmail.com>
2022-10-06 12:00:22 -0700

    All tests pass; rubocop/log resolved

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -3,12 +3,28 @@ module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
+    @data = []
+    @tsv_data = tsv.split("\n")
+    @keys = @tsv_data[0].split("\t")
+    @tsv_data.each do |line|
+      values = line.split("\t")
+      next if values[0] == 'date'
 
+      tsv_record = {}
+      @keys.each_index { |index| tsv_record[@keys[index]] = values[index] }
+      @data << tsv_record
+    end
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
-
+    # use any data in @data and return a String in TSV format.
+    @record = ''
+    @data[0].each { |key, _value| @record += key == 'tech_experience' ? "#{key}\n" : "#{key}\t" }
+    @data.each do |line|
+      line.each { |key, value| @record += key == 'tech_experience' ? "#{value}\n" : "#{value}\t" }
+    end
+    @record
   end
 end
--- yaml_buddy.rb
+++ yaml_buddy.rb
@@ -1,1 +1,16 @@
 # Module that can be included (mixin) to take and output Yaml data
+module YamlBuddy
+  require 'yaml'
+  # take_yaml: converts a String with YAML data into @data
+  # parameter: yaml - a String in yaml format
+  def take_yaml(yaml)
+    @data = YAML.load(yaml)
+  end
+
+  # to_yaml: converts @data into yaml string
+  # returns: String in yaml format
+  def to_yaml
+    # converts any data in @data into a yaml string.
+    @data.to_yaml
+  end
+end
